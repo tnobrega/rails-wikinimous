385b70286cddf1241c4f0cb365bdf1fe72937ef4
THIAGO NOBREGA <tnobrega@gmail.com>
2017-04-12 10:13:06 -0300

    funções ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+30.times do |i|
+  Article.create(title: Faker::Name.name, content: Faker::ChuckNorris.fact)
+end
